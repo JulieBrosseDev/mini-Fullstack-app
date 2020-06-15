3bbdd1fcc8cac74f1cde6b3c26809e06369f768d
Julie Brosse <privatejuliedev@gmail.com>
2020-06-15 11:40:27 +0200

    nodemon ok

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,8 @@ const  express  =  require('express');
 const  bodyParser  =  require('body-parser');
 const  morgan  =  require('morgan');
 const  app  =  express();
+const connection = require('./helpers/db.js');
+
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended:  false }));
@@ -17,8 +19,21 @@ app.get("/", (req,res) => {
 
 
 app.post('/signup', (req, res) => {
-    res.send('I am in POST signup');
-  });
+    var data  = {
+        name: req.body.name,
+        lastname: req.body.lastname,
+        email: req.body.email,
+        password: req.body.password,
+    };
+    connection.query('INSERT INTO users SET ?', data, (err, response, fields) => {
+        if(err) {
+           throw err;
+        }else{
+            console.log("THE DATA WAS INSERTED !!!")
+            res.end();
+        }
+    });
+});
 
 /// in case path is not found, return the 'Not Found' 404 code
 app.use(function(req, res, next) {
@@ -29,7 +44,7 @@ app.use(function(req, res, next) {
 
 
 // launch the node server
-let  server  =  app.listen( process.env.PORT  ||  3000, function(){
+let  server  =  app.listen( process.env.PORT  ||  5000, function(){
     console.log('Listening on port '  +  server.address().port);
 });
 
